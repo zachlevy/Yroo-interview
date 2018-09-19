0651527f5bcc1ee9633f3a2b33dd4569b6841e47
Zachary Levy <zacharyalevy@gmail.com>
2018-09-18 21:08:53 -0400

    add more test

--- test.rb
+++ test.rb
@@ -1,8 +1,48 @@
 require_relative './mission'
+require_relative './rover'
 
+# mission 1
 filepath1 = "curiosity.txt"
 mission1 = Mission.new(filepath1)
+
 mission1.land
+
+puts "1 2 N" == mission1.locate[0]
+puts "3 3 E" == mission1.locate[1]
+
 mission1.explore
+
+# ensure correct amount of rovers
+puts 2 == mission1.locate.length
+# rover 1 final destination
 puts "1 3 N" == mission1.locate[0]
+# rover 2 final destination
 puts "5 1 E" == mission1.locate[1]
+
+# ensure the map gets loaded
+puts "5 5" == mission1.map
+
+
+
+# mission 2
+# have rover move off of plateau
+
+filepath2 = "icarus.txt"
+mission2 = Mission.new(filepath2)
+
+mission2.land
+
+puts "5 5 N" == mission2.locate[0]
+
+begin
+  mission2.explore
+  puts false
+rescue => e
+  puts Rover::RoverOffPlateau == e.class
+end
+
+
+
+
+# mission 3
+# weird commands
